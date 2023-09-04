e9344d8d3250e82fff84d470af59a38b019fed3c
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-09-04 13:29:03 +0200

    day 2 work added

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -1,37 +1,85 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { fetchFail, fetchStart, registerSuccess } from "../features/authSlice";
 import axios from "axios";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} from "../features/authSlice";
+
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useAuthCall = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { token } = useSelector(state => state.auth);
+
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  const login = async userInfo => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/auth/login/`,
+        userInfo
+      );
+      dispatch(loginSuccess(data));
+      toastSuccessNotify("Login performed");
+      navigate("/stock");
+      console.log(data);
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+toastErrorNotify("Login can not be performed");
+    }
+  };
+
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      // let headers = {
+      //   Authorization: `Token ${token}`,
+      // };
+      await axios.post(`${BASE_URL}account/auth/logout/`, null, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });// post isteği atılırken axios ikinci parametreyi body olarak kabul eder. O nedenle eğer body bilgisi yoksa ikinci parametreye null veya boş obje tanımlanabilir. 3.parametre de headers verileri gönderilir.
+      dispatch(logoutSuccess());
+      toastSuccessNotify("Logout performed");
+      navigate("/");
+    } catch (err) {
+      dispatch(fetchFail());
+      toastErrorNotify("Logout can not be performed");
+    }
+  };
 
   const register = async userInfo => {
     dispatch(fetchStart());
     try {
       const { data } = await axios.post(
-        "http://10002.fullstack.clarusway.com/account/register/",
+        `${BASE_URL}account/register/`,
         userInfo
       );
-      console.log(data);
       dispatch(registerSuccess(data));
-      navigate("/stock")
-    } catch (error) {
+      toastSuccessNotify("Register performed");
+      navigate("/stock");
+    } catch (err) {
       dispatch(fetchFail());
+      if (err.response.status === 400) {
+        for (const [key, value] of Object.entries(err.response.data)) {
+          toastErrorNotify(`${key}: ${value[0]}`);
+        }
+      } else {
+        toastErrorNotify("Register can not be performed");
+      }
     }
   };
-  return register;
+
+  return { login, register, logout };
 };
 
 export default useAuthCall;
 
-// https://react.dev/learn/reusing-logic-with-custom-hooks
-
-//! Birden fazla componentte aynı fonksiyona veya fonksiyonlara ihtiyacım varsa (fetch gibi) ve bu fonksiyonlar içerisinde hooklara ihtiyaç duyuyorsam dispatch,state gibi o zaman custom hook yazmak mantıklıdır.
-//* custom hooklar "use" kelimesiyle başlar.
-//+ custom hooklar jsx return etmez.
-//* custom hookslar parametre alabilirler.
-//? birden fazla değer veya fonksiyon paylaşabiliriz. Eğer tek bir değer veya fonskiyon paylaşacaksak return deger dememiz yeterli. Ama birden fazlaysa o zaman object içerisinde değerlerimi, fonksiyonlarımı paylaşabilirim.
-//? Tek değer paylaştığımızda kullancağımız componentte direk olarka çağırabiliriz. Ama birden fazla değer paylaşıyorsak kullanırken destructuring yapmalıyız.
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,42 +2,18 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
+import { Formik } from "formik";
 import image from "../assets/result.svg";
 import Grid from "@mui/material/Grid";
+import RegisterForm, { registerSchema } from "../components/RegisterForm";
 import { Link } from "react-router-dom";
-import { Box, Button } from "@mui/material";
-import { Form, Formik } from "formik";
-import * as Yup from "yup";
-import TextField from "@mui/material/TextField";
+import { Box } from "@mui/material";
+
 import useAuthCall from "../hooks/useAuthCall";
 
-//! Yup ile istediğimiz alanlara istediğimiz validasyon koşullarını oluşturuyoruz. Sonra oluşturduğumuz bu şemayı formike tanımlayarak kullanıyoruz. Böylelikle formik hem formumuzu yönetiyor hem de verdiğimiz validationSchema yı uyguluyor. Dikkat edilmesi gereken husus; formikte tanımladığımız initialValues daki keylerle, Yupta tanımladığımız keylerin aynı olması. Eğer bir harf bile farklı olsa o alanla ilgili yazdığınız validation çalışmaz.
-const SignupSchema = Yup.object().shape({
-  username: Yup.string().min(2).max(50, "Too Long!").required("Required"),
-  first_name: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  last_name: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"), // hata mesajını bu şekilde customize edebiliriz. Mesaj yazmazsak default mesajını gösterir.
-  password: Yup.string()
-    .min(8, "Minimum 8 character!")
-    .max(50, "Too Long!")
-    .matches(/\d+/, "Minimum 1 number!")
-    .matches(/[a-z]/, "Minimum 1 lowercase letter!")
-    .matches(/[A-Z]/, "Minimum 1 uppercase letter!")
-    .matches(/[!,?{}><%&$#£+-.]+/, "Minimum 1 special character!") //regex
-    .required("Required"),
-  password2: Yup.string()
-    .oneOf([Yup.ref("password")], "Should match with password")
-    .required(),
-});
-
 const Register = () => {
-  const register = useAuthCall();
+  const { register } = useAuthCall();
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -82,103 +58,13 @@ const Register = () => {
               password: "",
               password2: "",
             }}
-            validationSchema={SignupSchema}
+            validationSchema={registerSchema}
             onSubmit={(values, actions) => {
-              console.log(values);
               register(values);
-              actions.resetForm(); //initialValuse geri döndürür.
-            }}>
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-            }) => (
-              <Form>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-                  <TextField
-                    name="username"
-                    label="Username"
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.username && Boolean(errors.username)} // error: true or false
-                    helperText={touched.username && errors.username}
-                  />
-                  {/* error ve helperText propertyleri Textfield componentine ait propertyler. */}
-                  {/* mui textfield kullanmadığımzda <span>{touched.username && errors.username}</span> */}
-                  <TextField
-                    id="first_name"
-                    label="First Name"
-                    type="text"
-                    variant="outlined"
-                    name="first_name"
-                    value={values.first_name}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={touched.first_name && errors.first_name} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için
-                    error={touched.first_name && errors.first_name} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için
-                  />
-                  <TextField
-                    id="last_name"
-                    label="Last Name"
-                    type="text"
-                    variant="outlined"
-                    name="last_name"
-                    value={values.last_name}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={touched.last_name && errors.last_name} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için
-                    error={touched.last_name && errors.last_name} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için
-                  />
-                  <TextField
-                    id="email"
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={touched.email && errors.email} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için
-                    error={touched.email && errors.email} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için
-                  />
-                  <TextField
-                    id="password"
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={touched.password && errors.password} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için
-                    error={touched.password && errors.password} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için
-                  />
-                  <TextField
-                    id="password2"
-                    label="Confirm Password"
-                    type="password"
-                    variant="outlined"
-                    name="password2"
-                    value={values.password2}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    helperText={touched.password2 && errors.password2} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için
-                    error={touched.password2 && errors.password2} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için
-                  />
-                  <Button type="submit" variant="contained" size="large">
-                    Submit
-                  </Button>
-                </Box>
-              </Form>
-            )}
-          </Formik>
-
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+            component={props => <RegisterForm {...props} />}></Formik>
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/">Already have an account? Sign in</Link>
           </Box>
